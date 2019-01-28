e849c19b2f44e6c41b172bf98a026cad55891f0b
maysara <maysarax@users.noreply.github.com>
2019-01-28 14:53:08 +0200

    Create DNS_Client.sh

---
+++ DNS_Client.sh
@@ -0,0 +1,18 @@
+
+//edit file /etc/resolv.conf
+
+sudo nano /etc/resolv.conf
+
+
+//Test DNS Server
+
+//Run the following command to test Primary DNS server.
+
+dig master.xxxxx.local
+
+//Check Secondary server using command:
+
+dig slave.xxxx.local
+
+
+
